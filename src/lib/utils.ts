bec0ed34f6625e07ca686b652b8465a432a1aee9
Matteo Horvath <horvathandrasmate@gmail.com>
2025-03-28 10:53:27 +0100

    asd

--- src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-export function formatString(str: string, length: number) {
-  return str.length > length ? str.substring(0, length) + "..." : str;
-}
